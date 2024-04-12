0a746f72c163a57dc16cba71d5c33892cded36ce
IsharaDeshan6 <id20051106@gmail.com>
2024-04-13 01:14:12 +0530

    CallList.tsx edited

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,102 +1,103 @@
-import { cn } from '@/lib/utils';
-import { CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk';
-import React, { useState } from 'react'
+'use client';
+import { useState } from 'react';
+import {
+  CallControls,
+  CallParticipantsList,
+  CallStatsButton,
+  CallingState,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
+} from '@stream-io/video-react-sdk';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Users, LayoutList } from 'lucide-react';
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Layout, LayoutList, Users } from 'lucide-react';
-import { Item } from '@radix-ui/react-dropdown-menu';
-import { SearchParamsContext } from 'next/dist/shared/lib/hooks-client-context.shared-runtime';
-import {useRouter, useSearchParams} from 'next/navigation';
-import EndCallButton from './EndCallButton';
+} from './ui/dropdown-menu';
 import Loader from './Loader';
+import EndCallButton from './EndCallButton';
+import { cn } from '@/lib/utils';
 
-type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
-
-
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
-  const SearchParams = useSearchParams();
-  const isPersonalRoom = !!SearchParams.get('personal');
-  const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
-
-  const [showParticipants, setShowParticipants] = useState(false);
-  const {useCallCallingState} = useCallStateHooks();
-  const callingState = useCallCallingState();
+  const searchParams = useSearchParams();
+  const isPersonalRoom = !!searchParams.get('personal');
   const router = useRouter();
+  const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
+  const [showParticipants, setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
 
-if(callingState!== CallingState.JOINED ) return <Loader />
+  // for more detail about types of CallingState see: https://getstream.io/video/docs/react/ui-cookbook/ringing-call/#incoming-call-panel
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) return <Loader />;
 
   const CallLayout = () => {
     switch (layout) {
       case 'grid':
-        return <PaginatedGridLayout />
+        return <PaginatedGridLayout />;
       case 'speaker-right':
-        return <SpeakerLayout
-          participantsBarPosition="left" />
+        return <SpeakerLayout participantsBarPosition="left" />;
       default:
-        return <SpeakerLayout
-          participantsBarPosition="right" />
+        return <SpeakerLayout participantsBarPosition="right" />;
     }
-  }
+  };
 
   return (
-    <section className='relative h-screen  w-full overflow-hidden  pt-4  text-white'>
-      <div className='relative flex size-full  items-center  justify-center'>
-        <div className='flex size-full max-w-[1000px] items-center'>
-          <CallLayout />
-        </div>
-        <div className={cn('h[calc(100vh-86px)] hidden ml-2', { 'shadow-black': showParticipants })}>
-          <CallParticipantsList onClose={() => setShowParticipants(false)} />
-        </div>
-      </div>
-      <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap'>
-        <CallControls onLeave={()=> router.push('/')}/>
-        <DropdownMenu>
-
-          <div className="flex items-center">
-            <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
-              <LayoutList size={20}
-                className='text-white ' />
-            </DropdownMenuTrigger>
+      <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+        <div className="relative flex size-full items-center justify-center">
+          <div className=" flex size-full max-w-[1000px] items-center">
+            <CallLayout />
           </div>
-
-          <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
-            {['Grid', 'Speaker-Left', 'Speaker-Right']
-              .map((item, index) => (
-                <div key={index}>
-
-
-                  <DropdownMenuItem className='cursor-pointer'
-                    onClick={() => {
-                      setLayout(item.toLocaleLowerCase() as CallLayoutType)
-                    }}>
-                    {item}
-
-                  </DropdownMenuItem>
-                  <DropdownMenuSeparator className='border-dark-1' />
-                </div>
-              ))
-            }
-
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <CallStatsButton />
-        <button onClick={() => setShowParticipants((prev) => !prev)}>
-          <div className='cursor-pointer rounded-2xl  bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
-            <Users size={20} className='text-white' />
+          <div
+              className={cn('h-[calc(100vh-86px)] hidden ml-2', {
+                'show-block': showParticipants,
+              })}
+          >
+            <CallParticipantsList onClose={() => setShowParticipants(false)} />
           </div>
-        </button>
-{!isPersonalRoom && <EndCallButton />}
+        </div>
+        {/* video layout and call controls */}
+        <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+          <CallControls onLeave={() => router.push(`/`)} />
 
-      </div>
-    </section>
-  )
-}
+          <DropdownMenu>
+            <div className="flex items-center">
+              <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+                <LayoutList size={20} className="text-white" />
+              </DropdownMenuTrigger>
+            </div>
+            <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+              {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+                  <div key={index}>
+                    <DropdownMenuItem
+                        onClick={() =>
+                            setLayout(item.toLowerCase() as CallLayoutType)
+                        }
+                    >
+                      {item}
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator className="border-dark-1" />
+                  </div>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <CallStatsButton />
+          <button onClick={() => setShowParticipants((prev) => !prev)}>
+            <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+              <Users size={20} className="text-white" />
+            </div>
+          </button>
+          {!isPersonalRoom && <EndCallButton />}
+        </div>
+      </section>
+  );
+};
 
 export default MeetingRoom;
--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,54 +1,38 @@
-"use client"
+'use client';
+
 import { ReactNode, useEffect, useState } from 'react';
+import { StreamVideoClient, StreamVideo } from '@stream-io/video-react-sdk';
+import { useUser } from '@clerk/nextjs';
+
 import { tokenProvider } from '@/actions/stream.actions';
 import Loader from '@/components/Loader';
-import { useUser } from '@clerk/nextjs';
-import {
-    StreamVideoClient,
-    StreamVideo,
 
-} from '@stream-io/video-react-sdk';
+const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-
-
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
-
-
-
-const StreamVideoProvider = ({children}: {
-    children:ReactNode
-}) => {
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
     const [videoClient, setVideoClient] = useState<StreamVideoClient>();
-   const {user, isLoaded} = useUser();
-
+    const { user, isLoaded } = useUser();
 
     useEffect(() => {
-        if(!isLoaded || !user) return;
-        if(!apiKey) throw new Error('Stream API key missing')
+        if (!isLoaded || !user) return;
+        if (!API_KEY) throw new Error('Stream API key is missing');
 
         const client = new StreamVideoClient({
-            apiKey,
-            user:{
-
+            apiKey: API_KEY,
+            user: {
                 id: user?.id,
-                name: user?.username ||  user?.id,
+                name: user?.username || user?.id,
                 image: user?.imageUrl,
-
-
             },
             tokenProvider,
-        })
+        });
+
         setVideoClient(client);
+    }, [user, isLoaded]);
 
-    },[user,isLoaded]);
-    
-    if(!videoClient) return <Loader />
+    if (!videoClient) return <Loader />;
 
-    return (
-        <StreamVideo client={videoClient}>
-            {children}
-        </StreamVideo>
-    );
+    return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
 
 export default StreamVideoProvider;
