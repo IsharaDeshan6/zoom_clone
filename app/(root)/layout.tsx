85226828e2200fda9b9617a48c09331b3a7622e5
IsharaDeshan6 <id20051106@gmail.com>
2024-04-12 23:56:07 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,26 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import React, { ReactNode } from 'react'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video Calling app",
+  icons:{
+    icon: '/icons/logo.svg'
+  }
+};
+
+const RootLayout = ({ children }: {
+  children:
+  ReactNode
+}) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default RootLayout
---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const protectedRoutes = createRouteMatcher([
+  '/',
+  '/upcoming',
+  '/meeting(.*)',
+  '/previous',
+  '/recordings',
+  '/personal-room',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoutes(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
